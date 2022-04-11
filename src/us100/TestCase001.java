4b12ad3f1ca0f768187e3de8cbd7835d12f9e932
hkarabey <99493517+hkarabey@users.noreply.github.com>
2022-04-11 21:25:30 +0300

    added hello word

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -4,6 +4,7 @@ public class TestCase001 {
     public static void main(String[] args) {
 
         System.out.println("hello word");
-        
+        System.out.println("hello word2");
+
     }
 }
